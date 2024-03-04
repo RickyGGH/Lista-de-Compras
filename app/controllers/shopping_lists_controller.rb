373088aa82c40418fd53c941acab8a82bcb3d545
Ricardo Gagliardo <ricardo_gagliardo@hotmail.com>
2024-03-04 18:40:45 -0300

    app en funcionamiento

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,30 +1,23 @@
 class ShoppingListsController < ApplicationController
   def index
-    # Obtener la fecha más reciente de los grupos de productos
     @earliest_date = Product.order(:purchase_date).limit(1).pluck(:purchase_date).first
 
-    # Obtener los productos asociados a esa fecha
     @earliest_products = Product.where(purchase_date: @earliest_date, purchased: false)
 
-    # Obtener otros grupos de productos
     @other_product_groups = Product.where.not(purchase_date: @earliest_date).where(purchased: false).group_by(&:purchase_date)
 
-    # Crear una instancia de producto para el formulario
     @product = Product.new
   end
 
   def create
-    # Verificar si se proporciona la fecha de compra, si no, establecerla en la fecha actual
     if product_params[:purchase_date].blank?
       product_params[:purchase_date] = Date.today
     end
 
     @product = Product.new(product_params)
     if @product.valid? && @product.save
-      # Redirigir a la raíz de la página para refrescar y mostrar el nuevo producto
       redirect_to root_path, notice: 'Product added successfully!'
     else
-      # Si hay errores de validación, renderizar de nuevo la página principal con los errores
       flash.now[:alert] = 'Invalid attributes. Please correct the errors.'
       render :index
     end
@@ -35,13 +28,12 @@ class ShoppingListsController < ApplicationController
     @product = Product.find(params[:id])
     if @product.update(purchased: true)
       puts "Product marked as purchased and updated successfully"
-      head :no_content # Devolver una respuesta exitosa
+      head :no_content
 
-      # Eliminar el producto de la base de datos después de actualizar el atributo 'purchased'
       @product.destroy
     else
       puts "Error updating product: #{@product.errors}"
-      head :bad_request # Devolver una respuesta de error
+      head :bad_request
     end
   end
 
--- app/javascript/shopping_list.js
+++ app/javascript/shopping_list.js
@@ -1,17 +1,13 @@
-// app/assets/javascripts/shopping_list.js
 document.addEventListener("click", function(event) {
   if (event.target.matches('.product-card input[type="checkbox"]')) {
     var checkbox = event.target;
     var productCard = checkbox.closest('.product-card');
-    productCard.style.display = 'none'; // Ocultar el producto cuando se marca el checkbox "Purchased"
+    productCard.style.display = 'none';
 
-    // Obtener el ID del producto
     var productId = productCard.dataset.productId;
 
-    // Verificar si el ID del producto es válido
     if (productId) {
-      // Realizar una solicitud AJAX para marcar el producto como comprado
-      fetch('/shopping_lists/' + productId, { // Cambiar a la ruta correcta
+      fetch('/shopping_lists/' + productId, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
@@ -19,7 +15,6 @@ document.addEventListener("click", function(event) {
         },
         body: JSON.stringify({ purchased: true })
       }).then(function(response) {
-        // Manejar la respuesta según sea necesario
       }).catch(function(error) {
         console.error('Error:', error);
       });
