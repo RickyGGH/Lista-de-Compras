0c7b47e26ed8a7ce82b8aa92ac66c6289d1942b4
Ricardo Gagliardo <ricardo_gagliardo@hotmail.com>
2024-03-04 00:05:37 -0300

    logrado renderizado de vistas y estilos

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,12 +1,12 @@
 class ShoppingListsController < ApplicationController
   def index
     # Obtener la fecha más reciente de los grupos de productos
-    @latest_date = Product.order(purchase_date: :desc).limit(1).pluck(:purchase_date).first
+    @earliest_date = Product.order(:purchase_date).limit(1).pluck(:purchase_date).first
 
     # Obtener los productos asociados a esa fecha
-    @latest_products = Product.where(purchase_date: @latest_date)
+    @earliest_products = Product.where(purchase_date: @earliest_date)
 
     # Obtener otros grupos de productos
-    @other_product_groups = Product.where.not(purchase_date: @latest_date).group_by(&:purchase_date)
+    @other_product_groups = Product.where.not(purchase_date: @earliest_date).group_by(&:purchase_date)
   end
 end
